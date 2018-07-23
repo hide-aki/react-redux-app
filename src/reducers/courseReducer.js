8680035b9ed17c62e3acb388bc4f98587cf83a26
Samuel Mussie <punisami@gmail.com>
2018-07-23 16:36:39 +0300

    changed to use the intial state constant

--- src/reducers/courseReducer.js
+++ src/reducers/courseReducer.js
@@ -1,6 +1,7 @@
 import * as ActionType from '../constants/actionTypes';
+import InitialState from '../constants/initialState';
 
-export default function courseReducer (state = [], action) {
+export default function courseReducer (state = InitialState.courses, action) {
     switch(action.type){
         case ActionType.CREATE_COURSE:
             return [...state, 
