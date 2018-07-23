b399f3b91c0988a3ea7ed85a0eada12e6e2a5f4e
Samuel Mussie <punisami@gmail.com>
2018-07-23 16:36:09 +0300

    furthere modifed course list so each course can be managed on another page

--- src/views/courses/CourseList.js
+++ src/views/courses/CourseList.js
@@ -1,30 +1,37 @@
-import React, {Component} from 'react';
-import { Row } from 'reactstrap';
+import React from 'react';
+import { Row, Table } from 'reactstrap';
+import Link from 'react-router-dom/Link'
 
-class CourseList extends Component {
-  
-  courseRow = (course, index) => {
-    return <tr className="" key={course.id}> <td>{course.title} </td> </tr>
-  }
-
-  render(){
-
+const courseRow = (course, index) => {
     return (
-        <div>
+        <tr className="" key={course.id}>
+            <td>
+                <Link to={`/courses/${course.id}`}>{course.title}</Link>
+            </td>
+        </tr>
+    );
+}
+
+const CourseList = ({courses}) => {
+    return (
+        <div className="container-fluid card">
             <Row>
             <h1>Courses</h1>
             </Row>
             <Row>
-                <table className="table">
-                    <tr>
-                        <th> Title </th>
-                    </tr>
-                    { this.props.courses.map(this.courseRow) }
-                </table>          
+                <Table responsive striped>
+                    <thead>
+                        <tr>
+                            <th> Title </th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        { courses.map(courseRow) }
+                    </tbody>
+                </Table>          
             </Row>
         </div>
     );
-  }
 }
 
 
