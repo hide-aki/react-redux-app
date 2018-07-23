070d2e9846e679bfb7d6d97c410c1f091ad16e2c
Samuel Mussie <punisami@gmail.com>
2018-07-23 12:40:12 +0300

    added thunk for async api call

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,8 +1,9 @@
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import rootReducer from '../reducers';
+import thunk from 'redux-thunk';
 
 export default function configureStore(initialState) {
     return createStore(
-        rootReducer, initialState
+        rootReducer, initialState, applyMiddleware(thunk)
     );
 };
